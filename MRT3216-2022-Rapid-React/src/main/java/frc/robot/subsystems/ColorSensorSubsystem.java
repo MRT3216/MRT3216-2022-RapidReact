115ca32f5188d5c0888ce234d27561d3abdc7bae
Wesley Sullivan <55400703+wesSull@users.noreply.github.com>
2022-01-17 11:17:28 -0700

    Color Sensor
    -Added ColorSensorSubsystem
    -Added ColorSensor to Constants
    -Added ColorSensor to RobotMap

---
+++ MRT3216-2022-Rapid-React/src/main/java/frc/robot/subsystems/ColorSensorSubsystem.java
@@ -0,0 +1,52 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.settings.Constants;
+import frc.robot.settings.RobotMap;
+
+
+public class ColorSensorSubsystem extends SubsystemBase {
+    private ColorSensorV3 colorSens = new ColorSensorV3(RobotMap.ROBOT.SENSORS.colorSensor);
+    public ColorSensorSubsystem () {
+    }
+
+    private Color getColor () {
+        //  Weirdly, this "getColor" is actually returning a stabilized color value, not the raw input, as with
+        //  .getRawColor - this is perfect for our use though, as we're just looking for the "average" color
+        return colorSens.getColor();
+    }
+
+    public boolean isRed() {
+        if (inRange() && getColor().red> getColor().blue) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean isBlue() {
+        if (inRange()) {
+            return !isRed();
+        }
+        else {
+            return false;
+        }
+    }
+
+    private double getProximity() {
+        return colorSens.getProximity();
+    }
+
+    private boolean inRange() {
+        if (getProximity()> Constants.Sensors.ColorRange) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
