af2043ff0be412602a0aac758a95862d6500e607
Daniel Lande <dlande@mcps.k12.mt.us>
2022-03-07 17:54:08 -0700

    Corrected merge issues.

--- MRT3216-2022-Rapid-React/src/main/java/frc/robot/OI/OIUtils.java
+++ MRT3216-2022-Rapid-React/src/main/java/frc/robot/OI/OIUtils.java
@@ -1,5 +1,7 @@
 package frc.robot.OI;
 
+import frc.robot.settings.Constants.OI;
+
 public class OIUtils {
     private static double deadband(double value, double deadband) {
         if (Math.abs(value) > deadband) {
@@ -15,11 +17,17 @@ public class OIUtils {
     
       public static double modifyAxis(double value) {
         // Deadband
-        value = deadband(value, 0.05);
-    
+        value = deadband(value, OI.kJoystickDeadband);
+
         // Square the axis
         value = Math.copySign(value * value, value);
     
         return value;
-      }
+    }
+
+    public static double expo(double value) {
+        double adjValue = (1 - (100 - OI.kExpoConstant) / 100) * Math.pow(value, 3)
+
+        return adjValue;
+    }
 }
