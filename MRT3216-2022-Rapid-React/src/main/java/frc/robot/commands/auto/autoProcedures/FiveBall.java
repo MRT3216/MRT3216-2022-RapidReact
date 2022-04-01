c12834e285e379fd8cf333e5f52f01feb67908e9
Wesley Sullivan <55400703+wesSull@users.noreply.github.com>
2022-04-01 11:58:04 -0600

    intake now runs during entire auto routine

--- MRT3216-2022-Rapid-React/src/main/java/frc/robot/commands/auto/autoProcedures/FiveBall.java
+++ MRT3216-2022-Rapid-React/src/main/java/frc/robot/commands/auto/autoProcedures/FiveBall.java
@@ -1,9 +1,11 @@
 package frc.robot.commands.auto.autoProcedures;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.auto.AutoAimAndShoot;
 import frc.robot.commands.auto.GoFetch;
+import frc.robot.commands.shooter.RunIntake;
 import frc.robot.settings.Constants;
 import frc.robot.subsystems.ColorSensorSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -14,29 +16,32 @@ import frc.robot.subsystems.shooter.HopperSubsystem;
 import frc.robot.subsystems.shooter.IndexerSubsystem;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 
-public class FiveBall extends SequentialCommandGroup {
+public class FiveBall extends ParallelCommandGroup {
 
         public FiveBall(SwerveSubsystem swerveSystem, IndexerSubsystem indexerSystem,
                         ColorSensorSubsystem colorSensorSystem,
                         HopperSubsystem hopperSystem, IntakeSubsystem intakeSystem, ShooterSubsystem shooterSystem,
                         LimelightSubsystem limelightSystem, HoodSubsystem hoodSystem, double startDelay) {
                 super(
-                        new WaitCommand(Constants.Auto.kStartDelayTime),
-                        new GoFetch(swerveSystem, indexerSystem, hopperSystem, colorSensorSystem, intakeSystem,
-                                shooterSystem, "5Ball1.1"),
-                        new WaitCommand(Constants.Auto.kDriveToShootDelay),
-                        new AutoAimAndShoot(shooterSystem, indexerSystem, hopperSystem, colorSensorSystem, limelightSystem,
-                                swerveSystem, hoodSystem, 2),
-                        new GoFetch(swerveSystem, indexerSystem, hopperSystem, colorSensorSystem, intakeSystem,
-                                shooterSystem, "5Ball1.2"),
-                        new WaitCommand(Constants.Auto.kDriveToShootDelay),
-                        new AutoAimAndShoot(shooterSystem, indexerSystem, hopperSystem, colorSensorSystem, limelightSystem,
-                                swerveSystem, hoodSystem, 2),
-                        new GoFetch(swerveSystem, indexerSystem, hopperSystem, colorSensorSystem, intakeSystem,
-                                shooterSystem, "5Ball1.3"),
-                        new WaitCommand(Constants.Auto.kDriveToShootDelay),
-                        new AutoAimAndShoot(shooterSystem, indexerSystem, hopperSystem, colorSensorSystem, limelightSystem,
-                                swerveSystem, hoodSystem, 1)
+                        new SequentialCommandGroup(
+                                new WaitCommand(Constants.Auto.kStartDelayTime),
+                                new GoFetch(swerveSystem, indexerSystem, hopperSystem, colorSensorSystem, intakeSystem,
+                                        shooterSystem, "5Ball1.1"),
+                                new WaitCommand(Constants.Auto.kDriveToShootDelay),
+                                new AutoAimAndShoot(shooterSystem, indexerSystem, hopperSystem, colorSensorSystem, limelightSystem,
+                                        swerveSystem, hoodSystem, 2),
+                                new GoFetch(swerveSystem, indexerSystem, hopperSystem, colorSensorSystem, intakeSystem,
+                                        shooterSystem, "5Ball1.2"),
+                                new WaitCommand(Constants.Auto.kDriveToShootDelay),
+                                new AutoAimAndShoot(shooterSystem, indexerSystem, hopperSystem, colorSensorSystem, limelightSystem,
+                                        swerveSystem, hoodSystem, 2),
+                                new GoFetch(swerveSystem, indexerSystem, hopperSystem, colorSensorSystem, intakeSystem,
+                                        shooterSystem, "5Ball1.3"),
+                                new WaitCommand(Constants.Auto.kDriveToShootDelay),
+                                new AutoAimAndShoot(shooterSystem, indexerSystem, hopperSystem, colorSensorSystem, limelightSystem,
+                                        swerveSystem, hoodSystem, 1)
+                        ),
+                        new RunIntake(intakeSystem, () -> true)
 
                 );
         }
--- MRT3216-2022-Rapid-React/src/main/java/frc/robot/commands/auto/autoProcedures/HPTwoBall.java
+++ MRT3216-2022-Rapid-React/src/main/java/frc/robot/commands/auto/autoProcedures/HPTwoBall.java
@@ -1,9 +1,11 @@
 package frc.robot.commands.auto.autoProcedures;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.auto.AutoAimAndShoot;
 import frc.robot.commands.auto.GoFetch;
+import frc.robot.commands.shooter.RunIntake;
 import frc.robot.settings.Constants;
 import frc.robot.subsystems.ColorSensorSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -14,17 +16,22 @@ import frc.robot.subsystems.shooter.HopperSubsystem;
 import frc.robot.subsystems.shooter.IndexerSubsystem;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 
-public class HPTwoBall extends SequentialCommandGroup {
+public class HPTwoBall extends ParallelCommandGroup {
     public HPTwoBall(SwerveSubsystem swerveSystem, IndexerSubsystem indexerSystem,
                      ColorSensorSubsystem colorSensorSystem,
                      HopperSubsystem hopperSystem, IntakeSubsystem intakeSystem, ShooterSubsystem shooterSystem,
                      LimelightSubsystem limelightSystem, HoodSubsystem hoodSystem, Double startDelayTime) {
         super(
-                new WaitCommand(startDelayTime),
-                new GoFetch(swerveSystem, indexerSystem, hopperSystem, colorSensorSystem, intakeSystem,
-                        shooterSystem, "2Ball1.1"),
-                new WaitCommand(Constants.Auto.kDriveToShootDelay),
-                new AutoAimAndShoot(shooterSystem, indexerSystem, hopperSystem, colorSensorSystem,
-                        limelightSystem, swerveSystem, hoodSystem, 2));
+                new SequentialCommandGroup(
+
+                        new WaitCommand(startDelayTime),
+                        new GoFetch(swerveSystem, indexerSystem, hopperSystem, colorSensorSystem, intakeSystem,
+                                shooterSystem, "2Ball1.1"),
+                        new WaitCommand(Constants.Auto.kDriveToShootDelay),
+                        new AutoAimAndShoot(shooterSystem, indexerSystem, hopperSystem, colorSensorSystem,
+                                limelightSystem, swerveSystem, hoodSystem, 2)
+
+                ),
+                new RunIntake(intakeSystem, () -> true));
     }
 }
